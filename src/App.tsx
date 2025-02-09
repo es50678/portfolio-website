828a41fd5d1edf4da1e158b7d802388279677c28
eduardo soto <es50678@gmail.com>
2025-02-09 16:40:13 -0500

    add Greeting component and enhance App with animal listing features

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Greeting from './Greeting';
+
+function ListItem(props: { animal: string }) {
+  return <li>{props.animal}</li>;
+}
+
+function List(props: { animals: string[] }) {
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        return <ListItem key={animal} animal={animal} />;
+      })}
+    </ul>
+  );
+}
+
+function ListOfAnimalsWithL(props: { animals: string[] }) {
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        return animal.startsWith("L") ? <ListItem key={animal} animal={animal} /> : null;
+      })}
+    </ul>
+  )
+}
+
+function Animals() {
+  const animals = ['Dog', 'Cat', 'Elephant', 'Lion', 'Tiger', 'Leopard', 'Monkey', 'Giraffe'];
+
+  return (
+    <div>
+      <h1>Animals: </h1>
+      <List animals={animals} />
+      <h2>Animals that start with L: </h2>
+      <ListOfAnimalsWithL animals={animals} />
+    </div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -16,20 +54,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <Greeting />
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <Animals />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
